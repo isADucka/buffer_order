d3cdef41e56764e26710d12bfd40d139a3857c66
isADucka <2867025942@qq.com>
2023-01-06 22:00:30 +0800

    运行

---
+++ src/main/java/com/cyy/BufferOrderApplication.java
@@ -0,0 +1,12 @@
+package com.cyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BufferOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BufferOrderApplication.class, args);
+    }
+
+}
